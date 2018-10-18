c48c46cc3c13ed47d5432d2231597c9ccd25ff98
zhouy12 <zhouy12@rose-hulman.edu>
2018-10-17 23:01:23 -0400

    done

---
+++ new.py
@@ -0,0 +1,13 @@
+import rosegraphics as rg
+
+r = 66
+r2 = r
+center = rg.Point(90, 40)
+circle = rg.Circle(center, r)
+r = 35
+center.x = 200
+
+print(r)
+print(r2)
+print(center)
+print(circle)
--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  IMPLEMENTING CLASSES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Michelle.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
